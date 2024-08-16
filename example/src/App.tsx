02bba260f9e66b0ddb375852f639b8f23f1b9c5b
Vagner Barbosa <vagner.zaham@gmail.com>
2024-08-16 01:48:21 -0300

    feat: commit first

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,13 @@
 import { StyleSheet, View, Text } from 'react-native';
 import { multiply } from 'react-native-lib-builder-bob-vgn';
-
+import GayaButton from '../../src/components/Button/GayaButton';
 const result = multiply(3, 7);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <GayaButton />
     </View>
   );
 }
---
+++ src/components/Button/GayaButton.tsx
@@ -0,0 +1,11 @@
+import { View, Button } from 'react-native';
+
+const GayaButton = () => {
+  return (
+    <View>
+      <Button title="Botão de Teste" />
+    </View>
+  );
+};
+
+export default GayaButton;
